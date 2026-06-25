b375d43ef3723c57a381fb9fbae0c45b9dfc6405
Rajdev Yadav <rajdevyadavofficial@gmail.com>
2026-06-25 00:21:49 -0400

    login page added.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
 
-const Login = () => {
+export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { login } = useAuth();
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (login(username, password)) {
+      navigate("/");
+    } else {
+      alert("Invalid Credentials");
+    }
+  };
+
   return (
-    <div>Login</div>
-  )
-}
+    <form onSubmit={handleSubmit}>
+      <input
+        placeholder="Username"
+        onChange={(e) => setUsername(e.target.value)}
+      />
 
-export default Login
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button type="submit">Login</button>
+    </form>
+  );
+}
